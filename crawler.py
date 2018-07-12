ec83436a5bdac9314df38b591268e1d12049e0f0
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-07-13 00:36:24 +0900

    update

--- crawler.py
+++ crawler.py
@@ -10,11 +10,9 @@ from googleapiclient.discovery import build
 fileDir = os.path.dirname(os.path.realpath(__file__))
 
 parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--dir", type=str, default=os.path.join(fileDir, "images"), help="image directory")
+parser.add_argument("--dir", type=str, default=os.path.join(fileDir, "images"), help="image directory")
 parser.add_argument("--num", type=int, default=10, help="number of images")
-parser.add_argument("--interval", type=int, default=10000,
-                    help="interval in milliseconds")
+parser.add_argument("--interval", type=int, default=10000, help="interval in milliseconds")
 parser.add_argument("--queries", type=str, nargs="+", help="search queries")
 args = parser.parse_args()
 
